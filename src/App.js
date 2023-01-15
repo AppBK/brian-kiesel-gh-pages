4437e7acd87be90edaa870250d4df3d9cc247d66
Brian Kiesel <app_academy_student_bk@protonmail.com>
2023-01-14 17:02:36 -0800

    figured out how to do the spreading transition and added custom favicon to site

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 function App() {
   return (
-    <h1>Hello from App</h1>
+    <>
+    <div id="main-outer">
+      <div id="main-content">
+            <div id="transforming-name">
+              <div className="emerging" id="rising-name"><div id="name-from-above">BRIAN</div></div>
+              <div className="emerging" id="falling-name"><div id="name-from-below">KIESEL</div></div>
+            </div>
+      </div>
+    </div>
+    </>
   );
 }
 
