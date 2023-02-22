73c5ba743a6d5082f3238ea30637322551472269
Brian Kiesel <app_academy_student_bk@protonmail.com>
2023-02-21 19:27:23 -0800

    new curtains

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
+import { useRef, useState } from "react";
+
 function App() {
+  const curtains = useRef();
+  const curtainRod = useRef();
+
+  const openCurtains = () => {
+    curtains.current.playbackRate = 2.5;
+    curtains.current.play();
+  }
+
+  const [curtainHeight, setCurtainHeight] = useState('');
+
   return (
     <>
-    <div id="black-frost">
+    {/* <div id="black-frost">
       <video id="curtains-vid" autoplay="autoplay" loop="true" muted defaultmuted>
         <source id="red-curtains" src="/red-curtains-sc.mp4" type="video/mp4"></source>
       </video>
-    </div>
+autoplay="autoplay" loop="true"
+
+    </div> */}
     <div id="main-outer">
+      <div onClick={openCurtains} className="opener">
+        <button onClick={openCurtains} className="opener">PLAY</button>
+      </div>
       <div id="main-content">
-        <div id="transforming-name">
+        <div id="transforming-name" style={{opacity: "1"}}>
           <div className="emerging" id="rising-name"><div id="name-from-above">BRIAN</div></div>
           <div className="emerging" id="falling-name"><div id="name-from-below">KIESEL</div></div>
         </div>
+          <div id="black-frost" ref={curtainRod}>
+            <div className="curtain-cont">
+              <video id="curtains-vid" muted defaultmuted ref={curtains}>
+                <source id="red-curtains" src="/rc-to-mp4.m4v" type="video/mp4"></source>
+              </video>
+            </div>
+          </div>
         <div id="headshot-container">
-          <img id ="headshot" src="/Brian_Kiesel_Headshots_01.jpg"></img>
+            <img id="headshot" src="/Brian_Kiesel_Headshots_01.jpg" style={{ opacity: "1", zIndex: "10" }}></img>
         </div>
         {/* <div id="language-button"> */}
           {/* <div id="concave-left">
